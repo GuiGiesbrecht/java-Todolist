426eb8b1ad1e8425212745729afc889997864f06
Guilherme Duarte Giesbrecht <guidgiesbrecht@gmail.com>
2023-10-16 21:59:56 -0300

    Save user with encrypted password

--- src/main/java/br/com/guilhermedg/todolist/user/UserController.java
+++ src/main/java/br/com/guilhermedg/todolist/user/UserController.java
@@ -9,17 +9,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private IUserRepository userRepository;
-
-    @GetMapping("/")
-    public String getUser() {
-        return "User1";
-    }
 
     @PostMapping("/")
     public ResponseEntity create(@RequestBody UserModel userModel) {
@@ -29,6 +26,9 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username already exists");
         }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
